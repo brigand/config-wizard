6c0f675c29ca5597f56ad8086d3a22d8f6c9f30c
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-02-27 20:56:01 -0700

    starts embed code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const embedTarget = document.querySelector('[data-config-wizard-type]');
+const target = embedTarget || document.getElementById('config-wizard-default-root');
+
+if (embedTarget) {
+  document.documentElement.classList.add(`config-wizard-is-embed`);
+} else {
+  document.documentElement.classList.add(`config-wizard-not-embed`);
+}
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <App isEmbed={!!embedTarget} />,
+  target,
 );
