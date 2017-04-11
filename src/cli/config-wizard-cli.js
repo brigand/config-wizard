a9dd6eb3ea74859b9b2c6352b9c0ee28cd65a1e5
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-04-11 12:49:15 -0700

    sets up cli

---
+++ src/cli/config-wizard-cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+
+const {argv} = yargs.command(
+  'babelrc',
+  `generate a babelrc and install deps`,
+  {
+    edge: {
+      default: 'cutting',
+    },
+    framework: {
+      required: true,
+    },
+    support: {
+      required: true,
+    },
+  },
+  (argv) => {
+    console.log(`babelrc command not currently supported`);
+    process.exitCode = 1;
+  }
+)
+  .demandCommand(1, `Please specify a command`);
+
+// console.log(argv);
+
