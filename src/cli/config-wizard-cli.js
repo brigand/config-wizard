1380a08366c6b733b6d783c970bfb4c7c4f2ab3f
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-04-11 12:51:51 -0700

    formatting

--- src/cli/config-wizard-cli.js
+++ src/cli/config-wizard-cli.js
@@ -2,25 +2,26 @@
 
 const yargs = require('yargs');
 
-const {argv} = yargs.command(
-  'babelrc',
-  `generate a babelrc and install deps`,
-  {
-    edge: {
-      default: 'cutting',
+const {argv} = yargs
+  .command(
+    'babelrc',
+    `generate a babelrc and install deps`,
+    {
+      edge: {
+        default: 'cutting',
+      },
+      framework: {
+        required: true,
+      },
+      support: {
+        required: true,
+      },
     },
-    framework: {
-      required: true,
-    },
-    support: {
-      required: true,
-    },
-  },
-  (argv) => {
-    console.log(`babelrc command not currently supported`);
-    process.exitCode = 1;
-  }
-)
+    (argv) => {
+      console.log(`babelrc command not currently supported`);
+      process.exitCode = 1;
+    }
+  )
   .demandCommand(1, `Please specify a command`);
 
 // console.log(argv);
